e737348edc8803a96f4dd6d6375f7208ac36457d
Jay <jgoolsby@server1.nationalswingers.net>
2025-02-10 06:55:56 +0000

    Updated Dashboard

--- components/Client/Dashboard/Places/SortBy.jsx
+++ components/Client/Dashboard/Places/SortBy.jsx
@@ -2,8 +2,9 @@
 
 const SortBy = (props) => {
     
-  return <div className="">
-        
+  return <div className="p-4 bg-gray-100 rounded-lg shadow-md text-center">
+    <h2 className="text-lg sm:text-xl font-bold text-gray-800 mb-3">Sort By Neighborhood</h2>
+        <div className="flex flex-wrap justify-center gap-2">
         {
            props.neighborhoods.map( (n, i) => {
                 return <button onClick={() => props.sortBys('neighborhood', n)} key={i} 
@@ -15,7 +16,11 @@ const SortBy = (props) => {
                 </button>
            })
         }
+        </div>
         <br />
+        <div className="p-4 bg-gray-100 rounded-lg shadow-md text-center">
+            <h2 className="text-lg sm:text-xl font-bold text-gray-800 mb-3">Sort By Category</h2>
+            <div className="flex flex-wrap justify-center gap-2">
         {
             props.categories.map((c, i) => {
                 return <button onClick={() => props.sortBys('category', c)}  key={i} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-3 rounded-full  
@@ -25,6 +30,9 @@ const SortBy = (props) => {
                 </button>
             })
         }
+            </div>
+        </div>
+        
         
 
 
